ed1171194849b3cfb25a6c2130d41150f29146a2
kanji <kanji8147@gmail.com>
2026-05-31 18:38:07 +0900

    ReservationのEntityを作成

---
+++ backend/src/main/java/com/ykanji/reserveflow/entity/Reservation.java
@@ -0,0 +1,24 @@
+@Entity
+@Table(name = "reservations")
+public class Reservation extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @ManyToOne
+    @JoinColumn(name = "staff_id")
+    private Staff staff;
+
+    @ManyToOne
+    @JoinColumn(name = "menu_id")
+    private Menu menu;
+
+    private LocalDateTime startTime;
+
+    private LocalDateTime endTime;
+}
