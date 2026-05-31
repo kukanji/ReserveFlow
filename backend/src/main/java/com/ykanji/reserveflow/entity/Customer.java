1adad94a84bcfdf306eec868975951a20390c631
kanji <kanji8147@gmail.com>
2026-05-31 18:37:18 +0900

    CustomerのEntityを作成

---
+++ backend/src/main/java/com/ykanji/reserveflow/entity/Customer.java
@@ -0,0 +1,17 @@
+@Entity
+@Table(name = "customers")
+public class Customer extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String phoneNumber;
+
+    private String memo;
+
+    @OneToMany(mappedBy = "customer")
+    private List<Reservation> reservations;
+}
