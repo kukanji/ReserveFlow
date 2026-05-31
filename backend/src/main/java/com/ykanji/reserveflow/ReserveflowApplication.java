27522da1c0b9a80f23649075c41422d7f5673d4f
kanji <kanji8147@gmail.com>
2026-05-31 17:33:23 +0900

    Spring Bootのプロジェクトを作成

---
+++ backend/src/main/java/com/ykanji/reserveflow/ReserveflowApplication.java
@@ -0,0 +1,13 @@
+package com.ykanji.reserveflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReserveflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReserveflowApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ykanji/reserveflow/ReserveflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ykanji.reserveflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReserveflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
